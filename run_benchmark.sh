cf3412158833bc0c4d93dfd28ddc7f7255279247
Anirudh Buvanesh <anirudhb1102@gmail.com>
2023-04-16 07:12:32 +0000

    Adding configs

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,18 +1,18 @@
 dataset=$1
 project=ELIAS
 # Generate initial clustering with BOW features
-python elias_utils.py gen_cluster_A configs/${dataset}/elias-1.yaml --no_model true
+python elias_utils.py gen_cluster_A configs/${dataset}/elias-1-def-final.yaml --no_model true  
 # Train ELIAS-1 model for a small number (5) of epochs
-python train.py configs/${dataset}/elias-1.yaml --num_epochs 5
+python train.py configs/${dataset}/elias-1-def-final.yaml --num_epochs 5
 # Generate clusters again, this time with concatenated BOW features and dense embedding from partially trained ELIAS-1 model
-python elias_utils.py gen_cluster_A Results/${project}/${dataset}/ELIAS-1/config.yaml
+python elias_utils.py gen_cluster_A Results/${project}/${dataset}/elias-1-def-final/config.yaml
 # Train ELIAS-1 model with new clusters
-python train.py configs/${dataset}/elias-1.yaml
+python train.py configs/${dataset}/elias-1-def-final.yaml
 # Generate row-wise sparse approximate adjacency graph (A) between clusters and labels based on trained ELIAS-1 model
-python elias_utils.py gen_approx_A Results/${project}/${dataset}/ELIAS-1/config.yaml
+python elias_utils.py gen_approx_A Results/${project}/${dataset}/elias-1-def-final/config.yaml
 # Train ELIAS-2 model (it takes trained ELIAS-1 model and generated approximate A as initialization and trains them end-to-end)
-python train.py configs/${dataset}/elias-2.yaml
+python train.py configs/${dataset}/elias-2-def-final.yaml
 # Evaluate trained model
-python eval.py Results/${project}/${dataset}/ELIAS-2/config.yaml
+python eval.py Results/${project}/${dataset}/elias-2-def-final/config.yaml
 # Learn sparse ranker to re-rank top 100 predictions (only intended to get best precision numbers for paper, not very useful for retrieval or production)
-python elias_utils.py sparse_ranker Results/${project}/${dataset}/ELIAS-2/config.yaml
+python elias_utils.py sparse_ranker Results/${project}/${dataset}/elias-2-def-final/config.yaml
