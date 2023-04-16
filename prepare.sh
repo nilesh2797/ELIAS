3a12ac69351f76ad965ab304fcd0d968f8e1301b
Anirudh Buvanesh <anirudhb1102@gmail.com>
2023-04-16 09:48:55 +0000

    Misc. changes

--- prepare.sh
+++ prepare.sh
@@ -2,5 +2,5 @@ dataset=$1
 max_seq_len=$2
 token_type=$3
 
-python create_tokenized_files.py --data-path  /home/t-rahulchand/xc/t-abuvanesh/Baselines/Models/ELIAS/Datasets/${dataset}/raw/trn_X.txt --tf-max-len ${max_seq_len} --tf-token-type ${token_type}
-python create_tokenized_files.py --data-path /home/t-rahulchand/xc/t-abuvanesh/Baselines/Models/ELIAS/Datasets/${dataset}/raw/tst_X.txt --tf-max-len ${max_seq_len} --tf-token-type ${token_type}
+python create_tokenized_files.py --data-path  /home/t-abuvanesh/xc/t-abuvanesh/Baselines/Models/ELIAS/Datasets/${dataset}/raw/trn_X.txt --tf-max-len ${max_seq_len} --tf-token-type ${token_type}
+python create_tokenized_files.py --data-path /home/t-abuvanesh/xc/t-abuvanesh/Baselines/Models/ELIAS/Datasets/${dataset}/raw/tst_X.txt --tf-max-len ${max_seq_len} --tf-token-type ${token_type}
